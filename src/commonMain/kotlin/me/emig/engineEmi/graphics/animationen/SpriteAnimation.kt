9e81b52b4d33ce7c6993e896c6e0bb60d1067911
Nicolai Emig <nicolai.emig@amseegym.de>
2020-05-12 20:57:22 +0200

    0.72

--- src/commonMain/kotlin/me/emig/engineEmi/graphics/animationen/SpriteAnimation.kt
+++ src/commonMain/kotlin/me/emig/engineEmi/graphics/animationen/SpriteAnimation.kt
@@ -4,95 +4,53 @@ import com.soywiz.klock.TimeSpan
 import com.soywiz.klock.measureTime
 import com.soywiz.klock.milliseconds
 import com.soywiz.korge.view.Image
-import com.soywiz.korge.view.image
 import com.soywiz.korim.bitmap.Bitmap
-import com.soywiz.korim.bitmap.Bitmaps
 import com.soywiz.korim.bitmap.BmpSlice
 import com.soywiz.korim.bitmap.sliceWithSize
 import com.soywiz.korio.async.delay
-import com.soywiz.korio.file.std.resourcesVfs
 
 class SpriteAnimation(
-    var spriteView : Image,
-    val spriteMap: Bitmap,
-    val spriteWidth: Int = 16,
-    val spriteHeight: Int = 16,
-    val marginTop: Int = 0,
-    val marginLeft: Int = 0,
-    val columns: Int = 1,
-    val lines: Int = 1,
-    val offsetBetweenColumns: Int = 0,
-    val offsetBetweenLines: Int = 0,
-    val scale: Number = 1.0) {
+        val id : String = "",
+        private val spriteMap: Bitmap,
+        private val spriteWidth: Int = 16,
+        private val spriteHeight: Int = 16,
+        private val marginTop: Int = 0,
+        private val marginLeft: Int = 0,
+        private val columns: Int = 1,
+        private val rows: Int = 1,
+        private val offsetBetweenColumns: Int = 0,
+        private val offsetBetweenRows: Int = 0) {
 
-    private var spriteStack: MutableList<BmpSlice> = mutableListOf(spriteView.bitmap)
+    private var spriteStack: MutableList<BmpSlice> = mutableListOf()
     private var cycles = 0
     private var currentSpriteIndex = 0
     private var stop = false
+    var frameTime = 25.milliseconds
+    val spriteStackSize : Int
+        get() = spriteStack.size
+    val firstSprite : BmpSlice
+        get() = spriteStack[0]
 
     init{
-        spriteStack.removeAt(0) //remove transparent initializer
-        var line = 0
-        repeat(columns) { col ->
-            val resourceBitmap = spriteMap
-            spriteStack.add(
-                resourceBitmap.sliceWithSize(
-                    marginLeft + (spriteWidth + offsetBetweenColumns) * col,
-                    marginTop + (spriteHeight + offsetBetweenLines) * line,
-                    spriteWidth,
-                    spriteHeight
+        for (col in 0 until columns){
+            for (row in 0 until rows){
+                spriteStack.add(
+                    spriteMap.sliceWithSize(
+                        marginLeft + (spriteWidth + offsetBetweenColumns) * col,
+                        marginTop + (spriteHeight + offsetBetweenRows) * row,
+                        spriteWidth,
+                        spriteHeight
+                    )
                 )
-            )
-            if (col % columns == 0 && col != 0) {
-                line++
             }
         }
-        spriteView.bitmap=spriteStack[0]
+        //spriteView.bitmap=spriteStack[0]
     }
 
+    fun nextSprite() = spriteStack[(++currentSpriteIndex % spriteStack.size)]
 
-    fun nextSprite(){
-        spriteView.bitmap=spriteStack[(++currentSpriteIndex % spriteStack.size)]
-    }
+    fun previousSprite() =spriteStack[(--currentSpriteIndex % spriteStack.size)]
 
-    fun previousSprite(){
-        spriteView.bitmap=spriteStack[(--currentSpriteIndex % spriteStack.size)]
-    }
 
-    suspend fun playForDuration(duration: TimeSpan, spriteDisplayTime: TimeSpan = 25.milliseconds) {
-        stop = false
-        if (duration > 0.milliseconds) {
-            var timeCounter = duration
-            while (timeCounter > 0.milliseconds && !stop ) {
-                val measuredTime = measureTime {
-                    nextSprite()
-                    delay(spriteDisplayTime)
-                }
-                timeCounter -= measuredTime
-            }
-        }
-    }
 
-    suspend fun play(times: Int = 1, spriteDisplayTime: TimeSpan = 25.milliseconds) {
-        stop = false
-        val cycleCount = times*spriteStack.size
-        var cycles = 0
-        while (cycles < cycleCount && !stop) {
-            nextSprite()
-            delay(spriteDisplayTime)
-            cycles++
-        }
-    }
-
-    suspend fun playLooped(spriteDisplayTime: TimeSpan = 25.milliseconds) {
-        stop = false
-        while (!stop) {
-            nextSprite()
-            delay(spriteDisplayTime)
-        }
-    }
-
-    fun stop() {
-        stop = true
-    }
 }
