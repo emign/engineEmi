65968e3ef88b2e8d747191f72f0c9ff96153382d
Nicolai Emig <nicolai.emig@amseegym.de>
2020-05-15 23:37:40 +0200

    0.78 - Added DSL-like constructors for all View Classes - Reworked Sound.kt and Music.kt to return NativeSoundChannel which can be .awaited() - Reworked Sound.kt and Music.kt to support Int.playbacktimes

--- src/commonMain/kotlin/me/emig/engineEmi/audio/Audio.kt
+++ src/commonMain/kotlin/me/emig/engineEmi/audio/Audio.kt
@@ -3,6 +3,7 @@ package me.emig.engineEmi.audio
 import com.soywiz.klock.TimeSpan
 import com.soywiz.korau.sound.*
 import com.soywiz.korge.view.*
+import com.soywiz.korio.async.*
 import com.soywiz.korio.file.std.resourcesVfs
 import kotlinx.coroutines.*
 import me.emig.engineEmi.graphics.animationen.*
@@ -34,33 +35,29 @@ class Tone(val frequency: Double, val duration: TimeSpan) {
  * Unterstützt werden WAV und MP3 Dateien
  * @constructor
  */
-class Sound(filePath: String, looped: Boolean = false) : Audio(filePath = filePath, looped = looped)
+class Sound(filePath: String, looped: Boolean = false) : Audio(filePath = filePath)
 
 /**
  * Spielt Musik aus der Datei [filePath] ab.
  * Unterstützt werden WAV und MP3 Dateien
  * @constructor
  */
-class Music(filePath: String, looped: Boolean = false) : Audio(filePath = filePath, streaming = true, looped = looped)
+class Music(filePath: String, looped: Boolean = false) : Audio(filePath = filePath, streaming = true)
 
-abstract class Audio(val filePath: String, val streaming: Boolean = false, var looped : Boolean = false) {
+abstract class Audio(val filePath: String, val streaming: Boolean = false) {
     private lateinit var sound: NativeSound
     private lateinit var channel: NativeSoundChannel
-    fun play() {
-        CoroutineScope(Dispatchers.Default).launch {
+    suspend fun play(playbackTimes: PlaybackTimes) : NativeSoundChannel {
             sound = resourcesVfs[filePath].readSound()
-            channel = sound.play()
-            channel.await()
-        }.invokeOnCompletion {
-            if (looped)
-                play()
-        }
+            val test = PlaybackTimes
+            return sound.play(playbackTimes)
     }
+
+    suspend fun play(times : Int) = play(times.playbackTimes)
 
     fun stop() {
         if (::channel.isInitialized){
             channel.stop()
-            looped = false
         }
 
     }
