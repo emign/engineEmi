88df3622528317c00c6a97622921f2d7022009be
Nicolai Emig <nicolai.emig@amseegym.de>
2020-05-18 14:37:07 +0200

    0.80 - Fixed View misplacement - Added Shape.kt - Added test.yml - Improved music loading - Fixed bug where a toplevel declared val could crash the scenechange

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     val korgePluginVersion: String by project
     repositories {
         maven { url = uri("https://plugins.gradle.org/m2/") }
-        maven { url = uri("https://dl.bintray.com/korlibs/korlibs") } // REMOVE
+        //maven { url = uri("https://dl.bintray.com/korlibs/korlibs") } // REMOVE
         mavenCentral()
 
     }
@@ -14,7 +14,7 @@ buildscript {
         classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.10.1")
         classpath("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.4")
-        classpath("com.soywiz.korlibs.korge.plugins:korge-gradle-plugin:$korgePluginVersion")
+        //classpath("com.soywiz.korlibs.korge.plugins:korge-gradle-plugin:$korgePluginVersion")
     }
 }
 
@@ -27,7 +27,7 @@ plugins {
 }
 
 
-apply<com.soywiz.korge.gradle.KorgeGradlePlugin>()
+//apply<com.soywiz.korge.gradle.KorgeGradlePlugin>()
 
 
 repositories {
