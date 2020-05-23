4ae1fdec5c274a2bfcfeac00ff30ad429adc7e36
Nicolai Emig <nicolai.emig@amseegym.de>
2020-05-23 15:36:43 +0200

    # 0.81a - 2020-05-23 - Added Korge 1.12.7     - This version includes a lot of optimizations, specially for Kotlin/Native     - Allow to have TileSets with several textures     - Allows to update Textures from Bitmaps without creating newbitmaps

--- src/commonTest/kotlin/Test.kt
+++ src/commonTest/kotlin/Test.kt
@@ -1,8 +1,11 @@
+import com.soywiz.klock.*
 import com.soywiz.korge.tests.*
 import com.soywiz.korge.tiled.*
 import com.soywiz.korge.view.*
+import com.soywiz.korim.bitmap.*
 import com.soywiz.korim.color.*
 import com.soywiz.korim.format.readBitmap
+import com.soywiz.korim.format.writeBitmap
 import com.soywiz.korio.file.std.*
 import com.soywiz.korio.lang.*
 import com.soywiz.korma.geom.*
@@ -10,7 +13,7 @@ import me.emig.engineEmi.addChildren
 import me.emig.engineEmi.graphics.shapes.*
 import kotlin.test.*
 
-class ShapesTest : ViewsForTesting() {
+class ViewsTest : ViewsForTesting() {
     @Test
     fun testrechteck() = viewsTest {
         val rechteck = Rechteck(33, 44, 111, 222, Colors.GREEN)
@@ -49,62 +52,6 @@ class ShapesTest : ViewsForTesting() {
     }
 
     @Test
-    fun testSpriteAnimation() = viewsTest {
-        val spriteMap = resourcesVfs["gfx/character/character.png"].readBitmap()
-        val spriteAnimationLeft = SpriteAnimation(
-            spriteMap = spriteMap,
-            spriteWidth = 16,
-            spriteHeight = 32,
-            marginTop = 96,
-            marginLeft = 1,
-            columns = 4,
-            rows = 1
-        )
-
-        val spriteAnimationRight = SpriteAnimation(
-            spriteMap = spriteMap,
-            spriteWidth = 16,
-            spriteHeight = 32,
-            marginTop = 32,
-            marginLeft = 1,
-            columns = 4,
-            rows = 1
-        )
-
-        val spriteAnimationUp = SpriteAnimation(
-            spriteMap = spriteMap,
-            spriteWidth = 16,
-            spriteHeight = 32,
-            marginTop = 64,
-            marginLeft = 1,
-            columns = 4,
-            rows = 1
-        )
-
-        val spriteAnimationDown = SpriteAnimation(
-            spriteMap = spriteMap,
-            spriteWidth = 16,
-            spriteHeight = 32,
-            marginTop = 0,
-            marginLeft = 1,
-            columns = 4,
-            rows = 1
-        )
-
-        val player1 = Sprite(spriteAnimationDown).apply {
-            scale(3)
-            xy(100,200)
-        }
-        val player2 = Sprite(spriteAnimationDown).apply {
-            scale(3)
-            xy(100,100)
-        }
-        addChildren(player1, player2)
-        assert(player1.isVisibleToUser())
-        assert(player2.isVisibleToUser())
-    }
-
-    @Test
     fun testTileMap() = viewsTest {
         val tiledMap = resourcesVfs["map/sample.tmx"].readTiledMap()
         fixedSizeContainer(width, height, clip = true) {
@@ -118,3 +65,6 @@ class ShapesTest : ViewsForTesting() {
 
     }
 }
+
+
+fun BmpSlice.extract(): Bitmap = bmp.extract(left, top, width, height)
